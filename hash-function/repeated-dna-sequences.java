b1e651865fa7aea589933d245beb3857ff2b2f67
William-egg <51187071+William-egg@users.noreply.github.com>
2025-04-18 18:27:45 +0800

    LC-repeated-dna-sequences [Runtime: N/A; Memory: N/A]

---
+++ hash-function/repeated-dna-sequences.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        Set<String> res = new HashSet<>();
+        int n = s.length();
+        Set<String> middle = new HashSet<>();
+        for(int i = 0;i+10<n;i++){
+            String tmp = s.substring(i,i+10);
+            if(middle.contains(tmp)&&!res.contains(tmp)){
+                res.add(tmp);
+            }
+            else{
+                middle.add(tmp);
+            }
+        }
+        return new ArrayList(res);
+    }
+}
